4b2f118fb205cea2db3454cb050b2510f5f2bdc4
RyujiShimatani <138374692+RyujiShimatani@users.noreply.github.com>
2023-07-05 01:50:16 +0000

    Finish Ruby8章

--- Fizz_Buzz.rb
+++ Fizz_Buzz.rb
@@ -1,18 +1,20 @@
-puts "数字を入力してください"
-input_num = gets
+def fizz_buzz(number)
+  if number % 15 == 0
+    "Fizz_Buzz"
+  elsif number % 5 == 0
+    "Buzz"
+  elsif number % 3 == 0
+    "Fizz"
+  else
+    number.to_s
+  end
 
-if input_num % 3 == 0
-  puts "結果は..."
-  puts "Fizz"
-elsif input_num % 5 == 0
-  puts "結果は..."
-  puts "Buzz"
-elsif input_num % 15 == 0
-  puts "結果は..."
-  puts "FizzBuzz"
-else
-  puts "結果は..."
-  puts input_num
+
 end
 
-  
+puts "数字を入力してください"
+
+input =gets.to_i
+
+puts "結果は..."
+puts fizz_buzz(input)
